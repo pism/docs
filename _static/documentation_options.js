f6f31edb41bb8c727ddb82c2ecdacb3df2555edc
CI Builder (PISM manual) <ckhroulev@alaska.edu>
2026-04-03 03:23:30 +0000

    Update the manual

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.3.0-79cae578d committed by Constantine Khrulev on 2026-03-22',
+    VERSION: '2.3.1-6664c8ae8 committed by Constantine Khrulev on 2026-04-02',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
